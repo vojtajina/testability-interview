013d096b82d1445626618ce671507cedc2661df0
Vojta Jina <vojta.jina@gmail.com>
2013-04-04 22:17:06 -0700

    5 add tests for batching send

--- example.spec.js
+++ example.spec.js
@@ -8,4 +8,45 @@ describe('Notifier', function() {
       expect(n.encodeMessage('hello')).toBe('secret:hello');
     });
   });
+
+  describe('send', function() {
+    it('should send a message to backend', function() {
+      var backendMock = jasmine.createSpyObj('Backend', ['sendBatch']);
+      var n = new Notifier(backendMock, 1);
+
+      var userDetails = {
+        email: 'me@domain.com'
+      };
+      var user = {
+        getDetails: function() {
+          return userDetails;
+        }
+      };
+
+      n.send(user, 'howdy');
+
+      expect(backendMock.sendBatch).toHaveBeenCalled();
+    });
+
+
+    it('should batch multiple message into a single call to the backend', function() {
+      var backendMock = jasmine.createSpyObj('Backend', ['sendBatch']);
+      var n = new Notifier(backendMock, 2);
+
+      var userDetails = {
+        email: 'me@domain.com'
+      };
+      var user = {
+        getDetails: function() {
+          return userDetails;
+        }
+      };
+
+      n.send(user, 'howdy');
+      expect(backendMock.sendBatch).not.toHaveBeenCalled();
+
+      n.send(user, 'again');
+      expect(backendMock.sendBatch).toHaveBeenCalled();
+    });
+  });
 });
